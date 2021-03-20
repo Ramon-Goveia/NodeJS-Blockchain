efd754702991a058b845cd9fe7a316ea7d33db82
Ramon-Goveia <ramonalangoveia@hotmail.com>
2021-03-20 15:12:07 -0300

    Terceiro commit - Primeiro bloco estatico

--- teste.js
+++ teste.js
@@ -2,5 +2,10 @@
 //Função para chamar o construtor
 const Block = require('./block.js')
 
-const block = new Block('242321', '2346233192d72d982ef', '623867drsfra1242', '100')
+const block = new Block(
+    '242321', 
+    '2346233192d72d982ef', 
+    '623867drsfra1242', 
+    '100'
+    )
 console.log(block.toString())
