68f724a38b661db21a0e1e092ea645a417edd114
Ramon-Goveia <ramonalangoveia@hotmail.com>
2021-03-20 14:37:51 -0300

    Segundo commit - class block create

---
+++ block.js
@@ -0,0 +1,24 @@
+//Classe
+class Block{
+    //Construtor
+    //Atributos
+    constructor(timestamp, lastHash, hash, data){
+        //vincular inputs aos atributos
+        this.timestamp = timestamp
+        this.lastHash = lastHash
+        this.hash = hash
+        this.data = data
+    }
+
+    /*Função de conversão da classe para string, 
+    para receber o valor dos inputs*/
+    toString(){
+        return `Block = 
+                Timestamp = ${this.timestamp}
+                lastHash = ${this.lastHash.substring(0, 10)}
+                hash = ${this.hash.substring(0, 10)}
+                data = ${this.data}
+                `
+    }
+
+}
